96df4acd0005969260a3afb125bffc60479b8888
代佳增 <daijiazeng@meituan.com>
2021-04-28 17:26:56 +0800

    feat(): 增加服务端代码

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
+
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'patch'
+  | 'PATCH'
